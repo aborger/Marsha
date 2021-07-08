ed428481397a84be11fc19499c58a9e82e8b5f2d
aborger <borger.aaron@gmail.com>
2021-07-08 16:26:22 -0600

    split ros

---
+++ ros_dev/nano/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/aaron/MARSHA/ros_dev/nano/catkin_ws/src
+# Build directory: /home/aaron/MARSHA/ros_dev/nano/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("marsha")
---
+++ ros_dev/nano/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH='/home/aaron/MARSHA/ros_dev/nano/catkin_ws/devel:/opt/ros/melodic'
+export LD_LIBRARY_PATH='/home/aaron/MARSHA/ros_dev/nano/catkin_ws/devel/lib:/opt/ros/melodic/lib'
+export PATH='/opt/ros/melodic/bin:/home/aaron/.vscode-server/bin/507ce72a4466fbb27b715c3722558bb15afa9f48/bin:/home/aaron/.vscode-server/bin/507ce72a4466fbb27b715c3722558bb15afa9f48/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/aaron/MARSHA/ros_dev/nano/catkin_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/aaron/MARSHA/ros_dev/nano/catkin_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH='/home/aaron/MARSHA/ros_dev/nano/catkin_ws/src:/opt/ros/melodic/share'
