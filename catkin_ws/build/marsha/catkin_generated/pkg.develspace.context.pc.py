b6e686e1b7fe8629ea065d57c60bc195799c8cd2
aborger <borger.aaron@gmail.com>
2021-07-16 20:07:39 -0600

    making repo package only

--- catkin_ws/build/marsha/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/aaron/MARSHA/ros_dev/nano/catkin_ws/devel/include".split(';') if "/home/aaron/MARSHA/ros_dev/nano/catkin_ws/devel/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "marsha"
-PROJECT_SPACE_DIR = "/home/aaron/MARSHA/ros_dev/nano/catkin_ws/devel"
-PROJECT_VERSION = "0.0.0"
--- catkin_ws/devel/share/marsha/cmake/marsha-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(marsha_MSG_INCLUDE_DIRS "/home/aaron/MARSHA/ros_dev/nano/catkin_ws/src/marsha/msg")
-set(marsha_MSG_DEPENDENCIES std_msgs;marsha)
