ec1c84def5ad93e3ac0257107260fa8dafb2c5c1
aborger <borger.aaron@gmail.com>
2021-09-04 10:46:57 -0600

    single arm move works

--- marsha_ai/nodes/move_interface.cpp
+++ marsha_ai/nodes/move_interface.cpp
@@ -22,22 +22,10 @@ class MarshaMoveInterface {
 
     void positionCallBack(const marsha_ai::Pos::ConstPtr& msg)
     {
-        ROS_INFO("Taking action[ x: %s y: %s z: %s", std::to_string(msg->x), std::to_string(msg->y), std::to_string(msg->z));
-        geometry_msgs::Pose target_pose1;
-        target_pose1.orientation.w = 1;
-        target_pose1.position.x = msg->x;
-        target_pose1.position.y = msg->y;
-        target_pose1.position.z = msg->z;
+        ROS_INFO("Taking action[ x: %f y: %f z: %f", msg->x, msg->y, msg->z);
+        
+        setPose(msg->x, msg->y, msg->z);
 
-        move_group->setPoseTarget(target_pose1);
-
-        moveit::planning_interface::MoveGroupInterface::Plan target_plan;
-
-        // Execute plan
-        bool success = (move_group->plan(target_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-        if (success) {
-            move_group->move();
-        }
     }
 
     public:
@@ -47,20 +35,21 @@ class MarshaMoveInterface {
         position_sub = nh->subscribe("pos_cmd", 1000, &MarshaMoveInterface::positionCallBack, this);
     }
 
-    void setPose()
+    void setPose(float x_val, float y_val, float z_val)
     {
         geometry_msgs::Pose target_pose1;
         target_pose1.orientation.w = 0.5;
         target_pose1.orientation.x = -0.5;
         target_pose1.orientation.y = -0.5;
         target_pose1.orientation.z = 0.5;
-        target_pose1.position.x = 0.50;
-        target_pose1.position.y = -0.40;
-        target_pose1.position.z = 0.30;
+        target_pose1.position.x = x_val;
+        target_pose1.position.y = y_val;
+        target_pose1.position.z = z_val;
         move_group->setPoseTarget(target_pose1);
 
         moveit::planning_interface::MoveGroupInterface::Plan target_plan;
 
+        ROS_INFO("Getting success...");
         bool success = (move_group->plan(target_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
         ROS_INFO("Plan status: %s", success ? "" : "FAILED");
         move_group->move();
@@ -75,8 +64,9 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "move_interface");
     ros::NodeHandle nh;
-    ros::AsyncSpinner spinner(1);
+    ros::AsyncSpinner spinner(4);
     spinner.start();
     MarshaMoveInterface interface = MarshaMoveInterface(&nh);
+    //interface.setPose();
     ros::waitForShutdown();
 }
