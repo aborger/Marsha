b7a5f0570f6efcc2d53eee8c42ed4e4107875f83
aborger <borger.aaron@gmail.com>
2021-10-19 08:11:57 -0600

    Can pass baton

--- marsha_ai/src/pick_place.cpp
+++ marsha_ai/src/pick_place.cpp
@@ -1,50 +1,10 @@
-/*********************************************************************
-* Software License Agreement (BSD License)
-*
-*  Copyright (c) 2012, Willow Garage, Inc.
-*  All rights reserved.
-*
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions
-*  are met:
-*
-*   * Redistributions of source code must retain the above copyright
-*     notice, this list of conditions and the following disclaimer.
-*   * Redistributions in binary form must reproduce the above
-*     copyright notice, this list of conditions and the following
-*     disclaimer in the documentation and/or other materials provided
-*     with the distribution.
-*   * Neither the name of Willow Garage nor the names of its
-*     contributors may be used to endorse or promote products derived
-*     from this software without specific prior written permission.
-*
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-*  POSSIBILITY OF SUCH DAMAGE.
-*********************************************************************/
-
-/* Author: Ioan Sucan, Ridhwan Luthra*/
-
-// ROS
 #include <ros/ros.h>
 
-// MoveIt
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 #include <moveit/move_group_interface/move_group_interface.h>
 
-// rand
 #include <stdlib.h>
 
-// TF2
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 #include "marsha_ai/GetAttachedState.h"
@@ -73,14 +33,14 @@ void openGripper(trajectory_msgs::JointTrajectory& posture)
   // BEGIN_SUB_TUTORIAL open_gripper
   /* Add both finger joints of panda robot. */
   posture.joint_names.resize(2);
-  posture.joint_names[0] = "panda_finger_joint1";
-  posture.joint_names[1] = "panda_finger_joint2";
+  posture.joint_names[0] = "actuator_joint_1";
+  posture.joint_names[1] = "actuator_joint_2";
 
   /* Set them as open, wide enough for the object to fit. */
   posture.points.resize(1);
   posture.points[0].positions.resize(2);
-  posture.points[0].positions[0] = 0.04;
-  posture.points[0].positions[1] = 0.04;
+  posture.points[0].positions[0] = 0.02;
+  posture.points[0].positions[1] = -0.02;
   posture.points[0].time_from_start = ros::Duration(0.5);
   // END_SUB_TUTORIAL
 }
@@ -90,14 +50,14 @@ void closedGripper(trajectory_msgs::JointTrajectory& posture)
   // BEGIN_SUB_TUTORIAL closed_gripper
   /* Add both finger joints of panda robot. */
   posture.joint_names.resize(2);
-  posture.joint_names[0] = "panda_finger_joint1";
-  posture.joint_names[1] = "panda_finger_joint2";
+  posture.joint_names[0] = "actuator_joint_1";
+  posture.joint_names[1] = "actuator_joint_2";
 
   /* Set them as closed. */
   posture.points.resize(1);
   posture.points[0].positions.resize(2);
-  posture.points[0].positions[0] = 0;
-  posture.points[0].positions[1] = 0;
+  posture.points[0].positions[0] = -0.0175;
+  posture.points[0].positions[1] = 0.0175;
   posture.points[0].time_from_start = ros::Duration(0.5);
   // END_SUB_TUTORIAL
 }
@@ -108,7 +68,7 @@ void pick(moveit::planning_interface::MoveGroupInterface& move_group)
   // Create a vector of grasps to be attempted, currently only creating single grasp.
   // This is essentially useful when using a grasp generator to generate and test multiple grasps.
   std::vector<moveit_msgs::Grasp> grasps;
-  grasps.resize(10);
+  grasps.resize(1);
 
   // Setting grasp pose
   // ++++++++++++++++++++++
@@ -118,59 +78,48 @@ void pick(moveit::planning_interface::MoveGroupInterface& move_group)
   // Therefore, the position for panda_link8 = 5 - (length of cube/2 - distance b/w panda_link8 and palm of eef - some
   // extra padding)
   
-  grasps[0].grasp_pose.header.frame_id = "panda_link0";
-  tf2::Quaternion orientation;
-  orientation.setRPY(-M_PI / 2, -M_PI / 4, -M_PI / 2);
-  grasps[0].grasp_pose.pose.orientation = tf2::toMsg(orientation);
-  
-  grasps[0].grasp_pose.pose.position.x = 0.415;
-  grasps[0].grasp_pose.pose.position.y = 0;
-  grasps[0].grasp_pose.pose.position.z = 0.5;
-  
-  for (int grasp = 1; grasp < 10; grasp++)
-  {
-    grasps[grasp].grasp_pose.header.frame_id = "panda_link0";
-    tf2::Quaternion orientation;
-    orientation.setRPY(-M_PI / 2, -M_PI / 4, -M_PI / 2);
-    grasps[grasp].grasp_pose.pose.orientation = tf2::toMsg(orientation);
+  grasps[0].grasp_pose.header.frame_id = "world_base";
 
-    grasps[grasp].grasp_pose.pose.position = generateGrasp(grasps[0].grasp_pose.pose.position);
-
+  grasps[0].grasp_pose.pose.orientation.x = -0.999911;
+  grasps[0].grasp_pose.pose.orientation.y = 0.006463;
+  grasps[0].grasp_pose.pose.orientation.z = -0.007255;
+  grasps[0].grasp_pose.pose.orientation.w = -0.009191;
+  
+  grasps[0].grasp_pose.pose.position.x = -0.25677;
+  grasps[0].grasp_pose.pose.position.y = -0.0545;
+  grasps[0].grasp_pose.pose.position.z = 0.1255;
   
 
+  // Setting pre-grasp approach
+  // ++++++++++++++++++++++++++
+  /* Defined with respect to frame_id */
+  grasps[0].pre_grasp_approach.direction.header.frame_id = "world_base";
+  /* Direction is set as positive x axis */
+  grasps[0].pre_grasp_approach.direction.vector.y = -1.0;
+  grasps[0].pre_grasp_approach.min_distance = 0.095;
+  grasps[0].pre_grasp_approach.desired_distance = 0.1; //0.115
+
+  // Setting post-grasp retreat
+  // ++++++++++++++++++++++++++
+  /* Defined with respect to frame_id */
+  grasps[0].post_grasp_retreat.direction.header.frame_id = "world_base";
+  /* Direction is set as positive z axis */
+  grasps[0].post_grasp_retreat.direction.vector.y = 1.0;
+  grasps[0].post_grasp_retreat.min_distance = 0.1;
+  grasps[0].post_grasp_retreat.desired_distance = 0.25;
+
+  // Setting posture of eef before grasp
+  // +++++++++++++++++++++++++++++++++++
+  openGripper(grasps[0].pre_grasp_posture);
+  // END_SUB_TUTORIAL
 
 
+  // BEGIN_SUB_TUTORIAL pick2
+  // Setting posture of eef during grasp
+  // +++++++++++++++++++++++++++++++++++
+  closedGripper(grasps[0].grasp_posture);
+  // END_SUB_TUTORIAL
 
-    // Setting pre-grasp approach
-    // ++++++++++++++++++++++++++
-    /* Defined with respect to frame_id */
-    grasps[grasp].pre_grasp_approach.direction.header.frame_id = "panda_link0";
-    /* Direction is set as positive x axis */
-    grasps[grasp].pre_grasp_approach.direction.vector.x = 1.0;
-    grasps[grasp].pre_grasp_approach.min_distance = 0.095;
-    grasps[grasp].pre_grasp_approach.desired_distance = 0.1; //0.115
-
-    // Setting post-grasp retreat
-    // ++++++++++++++++++++++++++
-    /* Defined with respect to frame_id */
-    grasps[grasp].post_grasp_retreat.direction.header.frame_id = "panda_link0";
-    /* Direction is set as positive z axis */
-    grasps[grasp].post_grasp_retreat.direction.vector.z = 1.0;
-    grasps[grasp].post_grasp_retreat.min_distance = 0.1;
-    grasps[grasp].post_grasp_retreat.desired_distance = 0.25;
-
-    // Setting posture of eef before grasp
-    // +++++++++++++++++++++++++++++++++++
-    openGripper(grasps[grasp].pre_grasp_posture);
-    // END_SUB_TUTORIAL
-
-
-    // BEGIN_SUB_TUTORIAL pick2
-    // Setting posture of eef during grasp
-    // +++++++++++++++++++++++++++++++++++
-    closedGripper(grasps[grasp].grasp_posture);
-    // END_SUB_TUTORIAL
-  }
 
   // BEGIN_SUB_TUTORIAL pick3
   // Set support surface as table1.
@@ -193,7 +142,7 @@ void place(moveit::planning_interface::MoveGroupInterface& group)
 
   // Setting place location pose
   // +++++++++++++++++++++++++++
-  place_location[0].place_pose.header.frame_id = "panda_link0";
+  place_location[0].place_pose.header.frame_id = "world_base";
   tf2::Quaternion orientation;
   orientation.setRPY(0, 0, M_PI / 2);
   place_location[0].place_pose.pose.orientation = tf2::toMsg(orientation);
@@ -206,7 +155,7 @@ void place(moveit::planning_interface::MoveGroupInterface& group)
   // Setting pre-place approach
   // ++++++++++++++++++++++++++
   /* Defined with respect to frame_id */
-  place_location[0].pre_place_approach.direction.header.frame_id = "panda_link0";
+  place_location[0].pre_place_approach.direction.header.frame_id = "world_base";
   /* Direction is set as negative z axis */
   place_location[0].pre_place_approach.direction.vector.z = -1.0;
   place_location[0].pre_place_approach.min_distance = 0.095;
@@ -215,7 +164,7 @@ void place(moveit::planning_interface::MoveGroupInterface& group)
   // Setting post-grasp retreat
   // ++++++++++++++++++++++++++
   /* Defined with respect to frame_id */
-  place_location[0].post_place_retreat.direction.header.frame_id = "panda_link0";
+  place_location[0].post_place_retreat.direction.header.frame_id = "world_base";
   /* Direction is set as negative y axis */
   place_location[0].post_place_retreat.direction.vector.y = -1.0;
   place_location[0].post_place_retreat.min_distance = 0.1;
@@ -245,7 +194,7 @@ void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface& pla
 
   // Add the first table where the cube will originally be kept.
   collision_objects[0].id = "table1";
-  collision_objects[0].header.frame_id = "panda_link0";
+  collision_objects[0].header.frame_id = "world_base";
 
   /* Define the primitive and its dimensions. */
   collision_objects[0].primitives.resize(1);
@@ -253,13 +202,13 @@ void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface& pla
   collision_objects[0].primitives[0].dimensions.resize(3);
   collision_objects[0].primitives[0].dimensions[0] = 0.2;
   collision_objects[0].primitives[0].dimensions[1] = 0.4;
-  collision_objects[0].primitives[0].dimensions[2] = 0.4;
+  collision_objects[0].primitives[0].dimensions[2] = 0.05;
 
   /* Define the pose of the table. */
   collision_objects[0].primitive_poses.resize(1);
-  collision_objects[0].primitive_poses[0].position.x = 0.5;
+  collision_objects[0].primitive_poses[0].position.x = -0.25;
   collision_objects[0].primitive_poses[0].position.y = 0;
-  collision_objects[0].primitive_poses[0].position.z = 0.2;
+  collision_objects[0].primitive_poses[0].position.z = 0.025;
   // END_SUB_TUTORIAL
 
   collision_objects[0].operation = collision_objects[0].ADD;
@@ -267,7 +216,7 @@ void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface& pla
   // BEGIN_SUB_TUTORIAL table2
   // Add the second table where we will be placing the cube.
   collision_objects[1].id = "table2";
-  collision_objects[1].header.frame_id = "panda_link0";
+  collision_objects[1].header.frame_id = "world_base";
 
   /* Define the primitive and its dimensions. */
   collision_objects[1].primitives.resize(1);
@@ -275,35 +224,35 @@ void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface& pla
   collision_objects[1].primitives[0].dimensions.resize(3);
   collision_objects[1].primitives[0].dimensions[0] = 0.4;
   collision_objects[1].primitives[0].dimensions[1] = 0.2;
-  collision_objects[1].primitives[0].dimensions[2] = 0.4;
+  collision_objects[1].primitives[0].dimensions[2] = 0.05;
 
   /* Define the pose of the table. */
   collision_objects[1].primitive_poses.resize(1);
-  collision_objects[1].primitive_poses[0].position.x = 0;
-  collision_objects[1].primitive_poses[0].position.y = 0.5;
-  collision_objects[1].primitive_poses[0].position.z = 0.2;
+  collision_objects[1].primitive_poses[0].position.x = 0.83;
+  collision_objects[1].primitive_poses[0].position.y = -0.03;
+  collision_objects[1].primitive_poses[0].position.z = 0.025;
   // END_SUB_TUTORIAL
 
   collision_objects[1].operation = collision_objects[1].ADD;
 
   // BEGIN_SUB_TUTORIAL object
   // Define the object that we will be manipulating
-  collision_objects[2].header.frame_id = "panda_link0";
+  collision_objects[2].header.frame_id = "world_base";
   collision_objects[2].id = "object";
 
   /* Define the primitive and its dimensions. */
   collision_objects[2].primitives.resize(1);
   collision_objects[2].primitives[0].type = collision_objects[1].primitives[0].BOX;
   collision_objects[2].primitives[0].dimensions.resize(3);
-  collision_objects[2].primitives[0].dimensions[0] = 0.02;
-  collision_objects[2].primitives[0].dimensions[1] = 0.02;
-  collision_objects[2].primitives[0].dimensions[2] = 0.2;
+  collision_objects[2].primitives[0].dimensions[0] = 0.04;
+  collision_objects[2].primitives[0].dimensions[1] = 0.2;
+  collision_objects[2].primitives[0].dimensions[2] = 0.04;
 
   /* Define the pose of the object. */
   collision_objects[2].primitive_poses.resize(1);
-  collision_objects[2].primitive_poses[0].position.x = 0.5;
+  collision_objects[2].primitive_poses[0].position.x = -0.25;
   collision_objects[2].primitive_poses[0].position.y = 0;
-  collision_objects[2].primitive_poses[0].position.z = 0.5;
+  collision_objects[2].primitive_poses[0].position.z = 0.077;
   // END_SUB_TUTORIAL
 
   collision_objects[2].operation = collision_objects[2].ADD;
@@ -320,14 +269,14 @@ bool grasped(ros::ServiceClient client) {
   }
   else
   {
-    ROS_ERROR("Failed to call service add_two_ints");
+    ROS_ERROR("Failed to call service response state");
     return false;
   }
 }
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "panda_arm_pick_place");
+  ros::init(argc, argv, "pick_place");
   ros::NodeHandle nh;
   ros::ServiceClient client = nh.serviceClient<marsha_ai::GetAttachedState>("grasp_status");
   
@@ -337,12 +286,13 @@ int main(int argc, char** argv)
 
   ros::WallDuration(1.0).sleep();
   moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-  moveit::planning_interface::MoveGroupInterface group("panda_arm");
+  moveit::planning_interface::MoveGroupInterface group("manipulator");
   group.setPlanningTime(45.0);
 
   addCollisionObjects(planning_scene_interface);
 
   // Wait a bit for ROS things to initialize
+
 
   while(!grasped(client))
   {
@@ -358,29 +308,9 @@ int main(int argc, char** argv)
     ros::WallDuration(1.0).sleep();
     place(group);
   }
+  
 
   ros::waitForShutdown();
   return 0;
 }
 
-// BEGIN_TUTORIAL
-// CALL_SUB_TUTORIAL table1
-// CALL_SUB_TUTORIAL table2
-// CALL_SUB_TUTORIAL object
-//
-// Pick Pipeline
-// ^^^^^^^^^^^^^
-// CALL_SUB_TUTORIAL pick1
-// openGripper function
-// """"""""""""""""""""
-// CALL_SUB_TUTORIAL open_gripper
-// CALL_SUB_TUTORIAL pick2
-// closedGripper function
-// """"""""""""""""""""""
-// CALL_SUB_TUTORIAL closed_gripper
-// CALL_SUB_TUTORIAL pick3
-//
-// Place Pipeline
-// ^^^^^^^^^^^^^^
-// CALL_SUB_TUTORIAL place
-// END_TUTORIAL
--- marsha_ai/src/task_test.cpp
+++
@@ -1,1 +0,0 @@
-//#include <moveit_task_constructor/task.h>
