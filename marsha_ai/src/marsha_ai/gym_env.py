db465c90e930b28aa0c914f0f3e30a95679771f9
aborger <borger.aaron@gmail.com>
2021-10-28 19:56:16 -0600

    can train AI

--- marsha_ai/src/marsha_ai/gym_env.py
+++ marsha_ai/src/marsha_ai/gym_env.py
@@ -20,7 +20,8 @@ class MarshaGym(gym.Env):
         # action[2] corresponds to z axis
         # The value of action element corresponds to the percent of a step to move
         self.action_space = spaces.Box(
-            low=-1, high=1, shape=(3,), dtype=np.float16)
+            low=0.8, high=1.2, shape=(4,), dtype=np.float16
+        )
 
         # Current observations: desired position(x, y, z)
         self.observation_space = spaces.Box(
@@ -28,16 +29,18 @@ class MarshaGym(gym.Env):
 
 
     def step(self, action):
-        self.ros_interface.perform_action(action)
+        done, reward = self.ros_interface.perform_action(action)
         self.current_step += 1
         obs = self.ros_interface.observe()
-        reward = 0
-        done = False
+        if self.current_step > 10:
+            done = True
+
 
         return obs, reward, done, {}
 
     def reset(self):
         self.ros_interface.reset_simulation()
+        self.current_step = 0
         return self.ros_interface.observe()
 
     def render(self, mode='', close=False):
--- marsha_ai/src/marsha_ai/ros_interface.py
+++ marsha_ai/src/marsha_ai/ros_interface.py
@@ -1,46 +1,136 @@
 #!/usr/bin/env python3
 
 import rospy
-from marsha_ai.msg import Pos
-import numpy as np
-from std_srvs.srv import Empty
-#import gazebo_msgs.ResetSimulation
 
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Point
+from marsha_ai.srv import MoveCmd
+from marsha_ai.srv import GetPose
+from marsha_ai.srv import PositionCmd
+import numpy as np
+from std_srvs.srv import Trigger, TriggerRequest
+
+from time import sleep
+
+import math
+
+
+
+def calculateDistance(p1, p2):
+    distance = math.sqrt((p2.x - p1.x)**2 + (p2.y - p1.y)**2 + (p2.z - p1.z)**2)
+    return distance
 
 class RosInterface():
     def __init__(self):
         rospy.init_node('AI_ros_interface')
         rospy.loginfo('Interface starting...')
 
-        self.pos_pub = rospy.Publisher('pos_cmd', Pos, queue_size=10)
-        self.rate = rospy.Rate(10)
+        rospy.wait_for_service('/left/position_cmd')
+        self.positionCmd = rospy.ServiceProxy('/left/position_cmd', PositionCmd)
 
+        rospy.wait_for_service('/left/get_pos')
+        self.getPos = rospy.ServiceProxy('/left/get_pos', GetPose)
+
+        rospy.wait_for_service('/reset')
+        self.reset = rospy.ServiceProxy('/reset', Trigger)
+
+        rospy.wait_for_service('/left/pose_cmd')
+        self.poseCmd = rospy.ServiceProxy('/left/pose_cmd', MoveCmd)
+
+        rospy.wait_for_service('/get_object_pos')
+        self.objectPos = rospy.ServiceProxy('/get_object_pos', GetPose)
+
+        rospy.wait_for_service('/left/grasp_cmd')
+        self.graspCmd = rospy.ServiceProxy('/left/grasp_cmd', MoveCmd)
+
+        rospy.wait_for_service('/left/is_grasped')
+        self.isGrasped = rospy.ServiceProxy('/left/is_grasped', Trigger)
+
+
+        rospy.logdebug('object_pos...')
+
+
+        rospy.loginfo('Interface Started!')
+
+    def _get_object_position(self):
+        object_pos = None
+        while object_pos == None:
+            try:
+                object_pos = self.objectPos().position
+            except KeyboardInterrupt:
+                break
+            except:
+                rospy.logwarn("Could not communicate!")
+                sleep(1)
+        return object_pos
 
     def perform_action(self, action):
-        msg = Pos()
-        msg.x = action[0]
-        msg.y = action[1]
-        msg.z = action[2]
-        self.pos_pub.publish(msg)
+        rospy.loginfo("Performing action...")
+        rospy.loginfo(action)
+        position = self.getPos().position
+        
+
+        if action[3] > 1.1:
+            self.graspCmd("open")
+        if action[3] < 0.9:
+            self.graspCmd("close")
+        
+        # move current position by NN output
+        position.x *= action[0]
+        position.y *= action[1]
+        position.z *= action[2]
+
+        # Move
+        success = self.positionCmd(position).done
+
+        rospy.loginfo("Action Success: " + str(success))
+
+        goal_position = Point(-0.25677, -0.0545, 0.115)
+
+
+        reward = 1 / calculateDistance(position, goal_position)
+
+        if success:
+            reward *= 2
+        else:
+            reward *= 0.5
+
+        grasped = self.isGrasped()
+
+        if grasped:
+            reward *= 10
+
+        rospy.loginfo("Reward: " + str(reward))
+        done = False
+        return done, reward
 
     def observe(self):
-        return np.zeros(shape=(3,1))
+        rospy.loginfo("Observing...")
+        position = self._get_object_position()
+
+        observation = np.zeros(shape=(3,1))
+        observation[0] = position.x
+        observation[1] = position.y
+        observation[2] = position.z
+
+        return observation
 
     
     def reset_simulation(self):
-        reset_act = [0.0, -0.40, -0.30]
-        self.perform_action(reset_act)
-        """
-        rospy.wait_for_service('/gazebo/reset_simulation')
-        try:
-            r = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
-            r()
-        except rospy.ServiceException as e:
-            rospy.logwarn('Reset service call failed: ', e)
-        """
+        rospy.loginfo("Resetting...")
+        success = self.poseCmd("preGrasp")
+        self.graspCmd("open")
+
+        self.reset(TriggerRequest())
+        
     
 
+def main():
+    interface = RosInterface()
 
+
+if __name__ == '__main__':
+    main()
 
 
 
