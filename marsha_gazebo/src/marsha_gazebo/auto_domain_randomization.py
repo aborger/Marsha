7895fa06452ed90f9cff19fab0ca35d0dfe0654a
aborger <borger.aaron@gmail.com>
2021-11-22 09:05:46 -0700

    Control hw robot with rviz and moveit

--- marsha_gazebo/src/marsha_gazebo/auto_domain_randomization.py
+++ marsha_gazebo/src/marsha_gazebo/auto_domain_randomization.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python
 # This file is called by "reset" to increase or decrese the difficulty of the task based on how good the previous episode's reward was
 import numpy as np
+import math
 
 class DRandomizer:
     def __init__(self, shape, range):
         self.shape = shape
         self.prev_reward = 0
         self.range = np.array(range)
-        self.difficulty = 0.1
+        self.advancement = -10
 
     def get_domain(self, reward):
         reward_gain = 0
-        if self.prev_reward != 0:
+        if self.prev_reward != 0 and reward > 0:
             reward_gain = (reward - self.prev_reward) / self.prev_reward
         self.prev_reward = reward
 
-        self.difficulty += reward_gain
+        self.advancement += reward_gain
+        difficulty = 1/(1+math.exp(-0.5*self.advancement)) # Sigmoid activation function to keep difficulty on interval (0, 1]
         print('reward:', reward, 'Prev_reward', self.prev_reward, 'reward_gain:', reward_gain)
-        print('Difficulty:', self.difficulty)
-        dar = self.range * self.difficulty # Difficulty Adjusted Range
+        print('Difficulty:', difficulty)
+        dar = self.range * difficulty # Difficulty Adjusted Range
         print('Diff Range:', dar)
         domain = (dar[1] - dar[0]) * np.random.random_sample(self.shape) + dar[0]
-        return domain, dar
+        return domain, difficulty
     
