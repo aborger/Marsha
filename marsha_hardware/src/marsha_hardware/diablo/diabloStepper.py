60c29d43ba4fcfb6cc483e461a74ce5347a7ca2d
aborger <borger.aaron@gmail.com>
2021-11-28 16:13:43 -0700

    Started on serial driver, implemented interactive marker.

--- marsha_hardware/src/marsha_hardware/diablo/diabloStepper.py
+++ marsha_hardware/src/marsha_hardware/diablo/diabloStepper.py
@@ -1,117 +1,121 @@
 #!/usr/bin/env python
 
-###
-#
-# diabloStepper.py: A script for controlling a stepper motor with the Diablo based on user input.
-#
-# 2019-04-26
-#
-###
+# Unfortunately the AR3 has one stepper that requires a current higher than the big easy controllers can produce.
 
-# Import library functions we need
-from __future__ import print_function, division
+# A diablo motor controller is being used instead.
+
+# This file replicates the Arduino Stepper class library written by Aaron, but for the diablo which was also unfortunately features drivers written in python.
+
+# It is based on the diabloStepper.py example at https://github.com/piborg/diablo/blob/master/diabloStepper.py
+
+# Author: Aaron Borger unfortunately...
+
 from diablo import *
 from time import sleep
-from sys import exit
 
-# Tell the system how to drive the stepper
-voltageIn = 12
-stepperCurrent = 2.8
-stepperResistance = 0.9
 
-driveLevel = 1.0
-holdLevel = 0.0
-stepDelay = 0.01
+DRIVE_LEVEL = 1.0
+HOLD_LEVEL = 0.0
+TOLERANCE = 1
 
-# Calculate our maxPower and holdingPower
-if(type(voltageIn) is str or type(stepperCurrent) is str or type(stepperResistance) is str):
-    print('Please set the voltageIn, stepperCurrent and stepperResistance to appropriate values in the script.')
-    exit()
 
-voltageOut = float(stepperCurrent) * float(stepperResistance)
-stepperPower = voltageOut / float(voltageIn)
-maxPower = driveLevel * stepperPower
-holdingPower = holdLevel * stepperPower
+class Stepper:
+    def __init__(self, voltageIn, stepperCurrent, stepperResistance):
 
-sequence = [                            # Order for stepping
-        [+maxPower, +maxPower],
-        [+maxPower, -maxPower],
-        [-maxPower, -maxPower],
-        [-maxPower, +maxPower]] 
-sequenceHold = [                        # Order for stepping at holding power
-        [+holdingPower, +holdingPower],
-        [+holdingPower, -holdingPower],
-        [-holdingPower, -holdingPower],
-        [-holdingPower, +holdingPower]]
+        self.stepDelay = 0.01 # Default, can be changed with set_speed()
+        self.seq_step = -1
 
-# Name the global variables
-global step
-global DIABLO
+        self.current_step = 0
+        self.desired_step = 0
+        
 
-# Set up the Diablo
-DIABLO = Diablo()
-#DIABLO.i2cAddress = 0x44                # Uncomment and change the value if you have changed the board address
-DIABLO.Init()
-if not DIABLO.foundChip:
-    boards = ScanForDiablo()
-    if len(boards) == 0:
-        print('No Diablo found, check you are attached :)')
-    else:
-        print('No Diablo at address %02X, but we did find boards:' % (DIABLO.i2cAddress))
-        for board in boards:
-            print('    %02X (%d)' % (board, board))
-        print('If you need to change the I2C address change the set-up line so it is correct, e.g.')
-        print('DIABLO.i2cAddress = 0x%02X' % (boards[0]))
-    exit()
-#DIABLO.SetEpoIgnore(True)              # Uncomment to disable EPO latch, needed if you do not have a switch / jumper
-DIABLO.ResetEpo()
-step = -1
+        voltageOut = float(stepperCurrent) * float(stepperResistance)
+        stepperPower = voltageOut / float(voltageIn)
+        maxPower = DRIVE_LEVEL * stepperPower
+        holdingPower = HOLD_LEVEL * stepperPower
 
-# Function to perform a sequence of steps as fast as allowed
-def MoveStep(count):
-    global step
-    global DIABLO
+        self.sequence = [                            # Order for stepping
+                    [+maxPower, +maxPower],
+                    [+maxPower, -maxPower],
+                    [-maxPower, -maxPower],
+                    [-maxPower, +maxPower]] 
+        self.sequenceHold = [                        # Order for stepping at holding power
+                    [+holdingPower, +holdingPower],
+                    [+holdingPower, -holdingPower],
+                    [-holdingPower, -holdingPower],
+                    [-holdingPower, +holdingPower]]
 
-    # Choose direction based on sign (+/-)
-    if count < 0:
-        dir = -1
-        count *= -1
-    else:
-        dir = 1
+        self.DIABLO = Diablo()
+        #DIABLO.i2cAddress = 0x44                # Uncomment and change the value if you have changed the board address
+        self.DIABLO.Init()
+        if not self.DIABLO.foundChip:
+            boards = ScanForDiablo()
+            if len(boards) == 0:
+                print('No Diablo found, check you are attached :)')
+            else:
+                print('No Diablo at address %02X, but we did find boards:' % (DIABLO.i2cAddress))
+                for board in boards:
+                    print('    %02X (%d)' % (board, board))
+                print('If you need to change the I2C address change the set-up line so it is correct, e.g.')
+                print('DIABLO.i2cAddress = 0x%02X' % (boards[0]))
+            exit()
+        #DIABLO.SetEpoIgnore(True)              # Uncomment to disable EPO latch, needed if you do not have a switch / jumper
+        self.DIABLO.ResetEpo()
 
-    # Loop through the steps
-    while count > 0:
-        # Set a starting position if this is the first move
-        if step == -1:
-            drive = sequence[-1]
-            DIABLO.SetMotor1(drive[0])
-            DIABLO.SetMotor2(drive[1])
-            step = 0
+    def set_point(self, step_position):
+        self.desired_step = step_position
+    
+    def get_current_step(self):
+        return self.current_step
+
+
+    def move(self, num_steps):
+        self.desired_step = self.current_step + num_steps
+        while (self.desired_step != self.current_step):
+            self.step()
+        
+
+    def step(self):
+        error = self.desired_step - self.current_step
+        if error < TOLERANCE and error > TOLERANCE * -1:
+            self.HoldPosition()
         else:
-            step += dir
+            print("Desired: ", self.desired_step, " Current: ", self.current_step, " Error: ", error)
+            # Choose direction based on sign (+/-)
+            if error < 0:
+                dir = -1
+                error *= -1
+            else:
+                dir = 1
 
-        # Wrap step when we reach the end of the sequence
-        if step < 0:
-            step = len(sequence) - 1
-        elif step >= len(sequence):
-            step = 0
+            # Set a starting position if this is the first move
+            if self.seq_step == -1:
+                drive = self.sequence[-1]
+                self.DIABLO.SetMotor1(drive[0])
+                self.DIABLO.SetMotor2(drive[1])
+                self.seq_step = 0
+            else:
+                self.seq_step += dir
 
-        # For this step set the required drive values
-        if step < len(sequence):
-            drive = sequence[step]
-            DIABLO.SetMotor1(drive[0])
-            DIABLO.SetMotor2(drive[1])
-        sleep(stepDelay)
-        count -= 1
+            # Wrap step when we reach the end of the sequence
+            if self.seq_step < 0:
+                self.seq_step = len(self.sequence) - 1
+            elif self.seq_step >= len(self.sequence):
+                self.seq_step = 0
 
-# Function to switch to holding power
-def HoldPosition():
-    global step
-    global DIABLO
+            # For this step set the required drive values
+            if self.seq_step < len(self.sequence):
+                drive = self.sequence[self.seq_step]
+                self.DIABLO.SetMotor1(drive[0])
+                self.DIABLO.SetMotor2(drive[1])
+            sleep(self.stepDelay)
+            self.current_step += dir
 
-    # For the current step set the required holding drive values
-    if step < len(sequence):
-        drive = sequenceHold[step]
-        DIABLO.SetMotor1(drive[0])
-        DIABLO.SetMotor2(drive[1])
+    # Function to switch to holding power
+    def HoldPosition(self):
+        # For the current step set the required holding drive values
+        if self.seq_step < len(self.sequence):
+            drive = self.sequenceHold[self.seq_step]
+            self.DIABLO.SetMotor1(drive[0])
+            self.DIABLO.SetMotor2(drive[1])
 
---
+++ marsha_hardware/src/marsha_hardware_driver.cpp
@@ -0,0 +1,11 @@
+#include <boost/asio.hpp>
+#include "marsha_hardware_driver.hpp"
+
+
+boost::asio::serial_port serial_port;
+
+int main(int argc, char** argv) {
+    
+}
+
+
