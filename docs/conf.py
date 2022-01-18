6e8fbec1a459cba51ae2fdeebc1409aa9c912968
Aaron Borger <46955176+aborger@users.noreply.github.com>
2022-01-18 12:26:24 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Marsha'
+copyright = '2022, Aaron Borger'
+author = 'Aaron Borger'
 
 release = '0.1'
 version = '0.1.0'
