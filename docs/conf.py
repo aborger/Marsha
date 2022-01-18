5169a90905fb7afd05119b120a7babc7307def4f
Aaron Borger <46955176+aborger@users.noreply.github.com>
2022-01-18 14:56:27 -0700

    Docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,12 @@
 # Configuration file for the Sphinx documentation builder.
 
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
+
 # -- Project information
 
 project = 'Marsha'
