068416ce87a99762da3943022a112d0eebbf9c8b
Aaron Borger <46955176+aborger@users.noreply.github.com>
2022-01-18 19:28:29 -0700

    Docs

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-html_logo = "_static/logo.png"
+html_logo = "_static/word_logo.png"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
