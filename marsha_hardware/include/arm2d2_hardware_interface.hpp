43f7d89de983d10440dbbee2d0355a6dc7b1515c
aborger <borger.aaron@gmail.com>
2022-03-05 12:47:23 -0700

    Added arm2d2 hardware

--- marsha_hardware/include/arm2d2_hardware_interface.hpp
+++ marsha_hardware/include/arm2d2_hardware_interface.hpp
@@ -1,14 +1,14 @@
-#ifndef MARSHA_HARDWARE_INTERFACE_H
-#define MARSHA_HARDWARE_INTERFACE_H
+#ifndef ARM2D2_HARDWARE_INTERFACE_H
+#define ARM2D2_HARDWARE_INTERFACE_H
 
 #include <ros/ros.h>
 #include <hardware_interface/joint_state_interface.h>
 #include <hardware_interface/joint_command_interface.h>
 #include <hardware_interface/robot_hw.h>
 #include <string.h>
-#include <std_msgs/Bool.h>
 
-#include <std_msgs/Int16MultiArray.h>
+#include <std_msgs/Int16.h>
+
 
 #define NUM_JOINTS 7
 
@@ -22,15 +22,18 @@ class MarshaArm : public hardware_interface::RobotHW
         void write();
         void update(const ros::TimerEvent &e);
 
-        void encoderCallBack(const std_msgs::Int16MultiArray &msg);
-        void graspCallBack(const std_msgs::Bool &msg);
+        //void encoderCallBack(const std_msgs::Int16MultiArray &msg);
+        //void graspCallBack(const std_msgs::Bool &msg);
     private:
         ros::NodeHandle nh;
-        ros::Publisher step_pub;
+        ros::Publisher j0_step_pub;
+        ros::Publisher j1_step_pub;
+        ros::Publisher j2_step_pub;
+        ros::Publisher j3_step_pub;
+        ros::Publisher j4_step_pub;
         ros::Publisher grip_pub;
-
         
-        ros::Subscriber grip_sub;
+        //ros::Subscriber grip_sub;
 
 
         hardware_interface::JointStateInterface joint_state_interface;
---
+++ marsha_hardware/src/arm2d2_hardware_interface.cpp
@@ -0,0 +1,106 @@
+#include "arm2d2_hardware_interface.hpp"
+
+
+
+MarshaArm::MarshaArm(ros::NodeHandle &nh_) {
+    nh = nh_;
+
+    //step_pub = nh.advertise<std_msgs::Int16MultiArray>("stepper_step", 10);
+    j0_step_pub = nh.advertise<std_msgs::Int16>("/steppers/J0/cmd", 10);
+    j1_step_pub = nh.advertise<std_msgs::Int16>("/steppers/J1/cmd", 10);
+    j2_step_pub = nh.advertise<std_msgs::Int16>("/steppers/J2/cmd", 10);
+    j3_step_pub = nh.advertise<std_msgs::Int16>("/steppers/J3/cmd", 10);
+    j4_step_pub = nh.advertise<std_msgs::Int16>("/steppers/J4/cmd", 10);
+    grip_pub = nh.advertise<std_msgs::Int16>("/steppers/grip/cmd", 10);
+    //ros::Subscriber enc_sub = nh.subscribe("encoder_feedback", 100, &MarshaArm::encoderCallBack, this);
+
+    // Note: This should be put in a loop for each controller
+
+    for (int i = 0; i < NUM_JOINTS - 1; i++) {
+        hardware_interface::JointStateHandle state_handle(joint_names[i], &pos[i], &vel[i], &eff[i]);
+        joint_state_interface.registerHandle(state_handle);
+
+        hardware_interface::JointHandle position_handle(state_handle, &cmd[i]);
+        joint_position_interface.registerHandle(position_handle);
+
+    }
+
+    int grip_id = NUM_JOINTS - 1;
+    hardware_interface::JointStateHandle gripper_state_handle(joint_names[grip_id], &pos[grip_id], &vel[grip_id], &eff[grip_id]);
+    joint_state_interface.registerHandle(gripper_state_handle);
+    hardware_interface::JointHandle gripper_effort_handle(gripper_state_handle, &cmd[grip_id]);
+    joint_effort_interface.registerHandle(gripper_effort_handle);
+
+    registerInterface(&joint_state_interface);
+    registerInterface(&joint_position_interface);
+    registerInterface(&gripper_effort_handle);
+
+    // Initialize output values
+    for (int i = 0; i < NUM_JOINTS; i++) {
+        pos[i] = 0.0;
+        vel[i] = 0.0;
+        eff[i] = 0.0;
+    }
+
+
+}
+
+void MarshaArm::read() {
+    // Read encoders currently done in encoderCallBack
+    //for(int i = 0; i < 6; i++) {
+
+    //}
+}
+
+void MarshaArm::write() {
+    float deg_per_steps = 1.8;
+
+    std_msgs::Int16 num_steps;
+    num_steps.data = int(radToDeg(cmd[0]) / deg_per_steps);
+    j0_step_pub.publish(num_steps);
+
+    num_steps.data = int(radToDeg(cmd[1]) / deg_per_steps);
+    j1_step_pub.publish(num_steps);
+
+    num_steps.data = int(radToDeg(cmd[2]) / deg_per_steps);
+    j2_step_pub.publish(num_steps);
+
+    num_steps.data = int(radToDeg(cmd[3]) / deg_per_steps);
+    j3_step_pub.publish(num_steps);
+
+    num_steps.data = int(radToDeg(cmd[4]) / deg_per_steps);
+    j4_step_pub.publish(num_steps);
+    //grip_pub.publish(cmd[0]);
+
+
+
+    /*
+    std_msgs::Int16MultiArray array; // can probably use Int16
+    std::vector<float> deg_per_steps;
+    ros::param::get("/ar3/stepper_config/deg_per_step", deg_per_steps);
+    for (int i = 0; i < NUM_JOINTS - 1; i++) {
+        int num_steps = int(radToDeg(cmd[i]) / deg_per_steps[i]); // deg/step for j1
+        array.data.push_back(num_steps);
+    }
+    ROS_INFO("J1: %f, J2: %f, J3: %f, J4: %f, J5: %f, J6: %f, grip: %f", cmd[0], cmd[1], cmd[2], cmd[3], cmd[4], cmd[5], cmd[6]);
+    step_pub.publish(array);*/
+}
+
+/*void MarshaArm::encoderCallBack(const std_msgs::Int16MultiArray &msg) {
+    // msg.data contains number of steps
+    std::vector<float> deg_per_steps;
+    ros::param::get("/ar3/stepper_config/deg_per_step", deg_per_steps);
+    for(int i = 0; i < NUM_JOINTS - 1; i++) {
+        pos[i] = msg.data[i] * deg_per_steps[i];
+    }
+}*/
+
+double MarshaArm::radToDeg(double rad) {
+	return rad / M_PI * 180.0;
+}
+
+double MarshaArm::degToRad(double deg) {
+    return deg / 180.0 * M_PI;
+}
+
+MarshaArm::~MarshaArm() {}
