e083cc664f7d7870f99ebc3c6f087c485b31499a
Aaron Borger <46955176+aborger@users.noreply.github.com>
2022-05-05 15:59:14 -0600

    Updated install

--- install_ros.bash
+++ install_ros.bash
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo Installing MARSHA...
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+yes | sudo apt install curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update
 yes | sudo apt install ros-melodic-ros-base
 yes | sudo apt install python3-pip python3-all-dev python3-rospkg
 yes | sudo apt install ros-melodic-ros-base --fix-missing
@@ -8,4 +13,17 @@ yes | sudo apt install ros-melodic-pcl-ros
 yes | sudo apt install ros-melodic-hardware-interface
 yes | sudo apt install ros-melodic-ros-control
 yes | sudo apt install ros-melodic-ros-controllers
+source /opt/ros/melodic/setup.bash
+mkdir -p ~/catkin_ws/src
+cd ~
+mv ~/marsha ~/catkin_ws/src
+cd ~/catkin_ws/src
+git clone https://github.com/aborger/ar3_core
+git clone https://github.com/ros/executive_smach.git -b indigo-devel
 
+echo -e "
+source ~/catkin_ws/devel/setup.bash \n
+export ROS_HOSTNAME=$HOSTNAME \n
+# Change which device is connected to \n
+export ROS_MASTER_URI=http://$HOSTNAME:11311 \n
+"
